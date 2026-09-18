1e13302f5c91053dbaef14fa3469a7b5067730d7
Михаил <60393588+bekodeg@users.noreply.github.com>
2026-09-18 14:46:50 +0400

    feat: add application architecture foundations

---
+++ lib/src/application/app_services.dart
@@ -0,0 +1,9 @@
+import 'use_cases/watch_user_budgets.dart';
+
+final class AppServices {
+  const AppServices({
+    required this.watchUserBudgets,
+  });
+
+  final WatchUserBudgets watchUserBudgets;
+}
