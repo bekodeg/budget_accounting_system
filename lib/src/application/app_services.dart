f3d6a600811d7cf03d7cad0767fe4e21bed87e9d
Михаил <60393588+bekodeg@users.noreply.github.com>
2026-09-23 11:45:41 +0400

    style: format Dart sources

--- lib/src/application/app_services.dart
+++ lib/src/application/app_services.dart
@@ -1,9 +1,7 @@
 import 'use_cases/watch_user_budgets.dart';
 
 final class AppServices {
-  const AppServices({
-    required this.watchUserBudgets,
-  });
+  const AppServices({required this.watchUserBudgets});
 
   final WatchUserBudgets watchUserBudgets;
 }
