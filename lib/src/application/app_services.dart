0634ae6f8f684acfe798a6442f679e50b3ac4a7c
Михаил <60393588+bekodeg@users.noreply.github.com>
2026-09-24 10:45:17 +0400

    feat: expose transaction services

--- lib/src/application/app_services.dart
+++ lib/src/application/app_services.dart
@@ -4,14 +4,19 @@ import 'use_cases/archive_category.dart';
 import 'use_cases/create_account.dart';
 import 'use_cases/create_category.dart';
 import 'use_cases/create_initial_budget.dart';
+import 'use_cases/create_transaction.dart';
+import 'use_cases/delete_transaction.dart';
 import 'use_cases/get_account_balance.dart';
 import 'use_cases/rename_category.dart';
 import 'use_cases/require_account_in_budget.dart';
+import 'use_cases/require_category_in_budget.dart';
 import 'use_cases/resolve_app_startup.dart';
 import 'use_cases/select_budget.dart';
 import 'use_cases/update_account.dart';
+import 'use_cases/update_transaction.dart';
 import 'use_cases/watch_budget_accounts.dart';
 import 'use_cases/watch_budget_categories.dart';
+import 'use_cases/watch_transactions.dart';
 import 'use_cases/watch_user_budgets.dart';
 
 final class AppServices {
@@ -22,14 +27,19 @@ final class AppServices {
     required this.createAccount,
     required this.createCategory,
     required this.createInitialBudget,
+    required this.createTransaction,
+    required this.deleteTransaction,
     required this.getAccountBalance,
     required this.renameCategory,
     required this.requireAccountInBudget,
+    required this.requireCategoryInBudget,
     required this.resolveAppStartup,
     required this.selectBudget,
     required this.updateAccount,
+    required this.updateTransaction,
     required this.watchBudgetAccounts,
     required this.watchBudgetCategories,
+    required this.watchTransactions,
     required this.watchUserBudgets,
   });
 
@@ -39,13 +49,18 @@ final class AppServices {
   final CreateAccount createAccount;
   final CreateCategory createCategory;
   final CreateInitialBudget createInitialBudget;
+  final CreateTransaction createTransaction;
+  final DeleteTransaction deleteTransaction;
   final GetAccountBalance getAccountBalance;
   final RenameCategory renameCategory;
   final RequireAccountInBudget requireAccountInBudget;
+  final RequireCategoryInBudget requireCategoryInBudget;
   final ResolveAppStartup resolveAppStartup;
   final SelectBudget selectBudget;
   final UpdateAccount updateAccount;
+  final UpdateTransaction updateTransaction;
   final WatchBudgetAccounts watchBudgetAccounts;
   final WatchBudgetCategories watchBudgetCategories;
+  final WatchTransactions watchTransactions;
   final WatchUserBudgets watchUserBudgets;
 }
